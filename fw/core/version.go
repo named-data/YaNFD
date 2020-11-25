13468edab49aabd118e5f730fdd0e5db2127df07
Eric Newberry <enewberry@cs.ucla.edu>
2020-11-24 23:48:13 -0700

    Add basic structure

---
+++ fw/core/version.go
@@ -0,0 +1,4 @@
+package core
+
+// Version of YaNFD
+const Version = "0.0.1"
---
+++ fw/fw/thread.go
@@ -0,0 +1,11 @@
+package fw
+
+// Thread Represents a forwarding thread
+type Thread struct {
+	ThreadID uint32
+}
+
+// Run forwarding thread
+func (thread Thread) Run() {
+	// TODO
+}
