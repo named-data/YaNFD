6b9d6fb09cab4dd9b3fb84f4cd403261e3e6c4ab
Eric Newberry <enewberry@cs.ucla.edu>
2020-12-20 14:30:05 -0700

    Add NDNLPv2 reassembly

--- face/ndnlp-link-service.go
+++ face/ndnlp-link-service.go
@@ -21,7 +21,8 @@ type ndnlpLinkServiceOptions struct {
 
 // NDNLPLinkService is a link service implementing the NDNLPv2 link protocol
 type NDNLPLinkService struct {
-	options ndnlpLinkServiceOptions
+	options             ndnlpLinkServiceOptions
+	partialMessageStore map[uint64][][]byte
 	linkServiceBase
 }
 
@@ -68,13 +69,90 @@ func (l *NDNLPLinkService) handleIncomingFrame(rawFrame []byte) {
 
 	core.LogDebug(l, "Received NDNLPv2 frame of size", len(rawFrame))
 
-	// Reassembly
-	if l.options.IsReassemblyEnabled {
+	// Reliability
+	if l.options.IsReliabilityEnabled {
+		// Process Acks
 		// TODO
-	} else if frame.FragCount() != nil {
+
+		// Add TxSequence to Ack queue
 		// TODO
 	}
 
-	// Hash to forwarding thread and place in queue
+	// Reassembly
+	netPkt := frame.Fragment()
+	if l.options.IsReassemblyEnabled && frame.Fragment() != nil {
+		if frame.Sequence() == nil {
+			core.LogInfo(l, "Received NDNLPv2 frame without Sequence but reassembly requires it - DROP")
+			return
+		}
+
+		fragIndex := uint64(0)
+		if frame.FragIndex() != nil {
+			fragIndex = *frame.FragIndex()
+		}
+		fragCount := uint64(1)
+		if frame.FragCount() != nil {
+			fragCount = *frame.FragCount()
+		}
+		baseSequence := *frame.Sequence() - fragIndex
+
+		core.LogDebug(l, "Received fragment", fragIndex, "of", fragCount, "for", baseSequence)
+
+		if fragIndex == 0 && fragCount == 1 {
+			// Bypass reassembly since only one fragment
+		} else {
+			netPkt = l.reassemblePacket(frame, baseSequence, fragIndex, fragCount)
+			if netPkt == nil {
+				// Nothing more to be done, so return
+				return
+			}
+		}
+	} else if frame.FragCount() != nil || frame.FragIndex() != nil {
+		core.LogWarn(l, "Received NDNLPv2 frame containing fragmentation fields but reassembly disabled - DROP")
+		return
+	}
+
+	// Hand off to network layer
+	// Which will hash to forwarding thread and place in queue based upon type
 	// TODO
+}
+
+func (l *NDNLPLinkService) reassemblePacket(frame *lpv2.Packet, baseSequence uint64, fragIndex uint64, fragCount uint64) *tlv.Block {
+	_, hasSequence := l.partialMessageStore[baseSequence]
+	if !hasSequence {
+		// Create map entry
+		l.partialMessageStore[baseSequence] = make([][]byte, fragCount)
+	}
+
+	// Insert into PartialMessageStore
+	l.partialMessageStore[baseSequence][fragIndex] = make([]byte, len(frame.Fragment().Value()))
+	copy(l.partialMessageStore[baseSequence][fragIndex], frame.Fragment().Value())
+
+	// Determine whether it is time to reassemble
+	receivedCount := 0
+	receivedTotalLen := 0
+	for _, fragment := range l.partialMessageStore[baseSequence] {
+		if len(fragment) != 0 {
+			receivedCount++
+			receivedTotalLen += len(fragment)
+		}
+	}
+
+	if receivedCount == len(l.partialMessageStore[baseSequence]) {
+		// Time to reassemble!
+		reassembled := new(tlv.Block)
+		reassembled.SetType(lpv2.Fragment)
+
+		reassembledValue := make([]byte, receivedTotalLen)
+		reassembledSize := 0
+		for _, fragment := range l.partialMessageStore[baseSequence] {
+			copy(reassembledValue[reassembledSize:], fragment)
+		}
+		reassembled.SetValue(reassembledValue)
+
+		delete(l.partialMessageStore, baseSequence)
+		return reassembled
+	}
+
+	return nil
 }
