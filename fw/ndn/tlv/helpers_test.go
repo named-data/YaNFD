53471b5d6ab8b43518b21cfef833d4e33151d3fa
Eric Newberry <enewberry@cs.ucla.edu>
2020-12-20 17:51:01 -0700

    Fix non-negative integer encoding

--- fw/ndn/tlv/helpers_test.go
+++ fw/ndn/tlv/helpers_test.go
@@ -83,3 +83,45 @@ func TestNNIBlock(t *testing.T) {
 	assert.NoError(t, err)
 	assert.ElementsMatch(t, nniWire, encodedWire)
 }
+
+func TestEncodeNNI(t *testing.T) {
+	nni1 := uint64(0x01)
+	octet1 := []byte{0x01}
+	block1 := tlv.EncodeNNIBlock(0xAA, nni1)
+	assert.ElementsMatch(t, octet1, block1.Value())
+
+	nni2 := uint64(0x0102)
+	octet2 := []byte{0x01, 0x02}
+	block2 := tlv.EncodeNNIBlock(0xAA, nni2)
+	assert.ElementsMatch(t, octet2, block2.Value())
+
+	nni3 := uint64(0x010203)
+	octet3 := []byte{0x00, 0x01, 0x02, 0x03}
+	block3 := tlv.EncodeNNIBlock(0xAA, nni3)
+	assert.ElementsMatch(t, octet3, block3.Value())
+
+	nni4 := uint64(0x01020304)
+	octet4 := []byte{0x01, 0x02, 0x03, 0x04}
+	block4 := tlv.EncodeNNIBlock(0xAA, nni4)
+	assert.ElementsMatch(t, octet4, block4.Value())
+
+	nni5 := uint64(0x0102030405)
+	octet5 := []byte{0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05}
+	block5 := tlv.EncodeNNIBlock(0xAA, nni5)
+	assert.ElementsMatch(t, octet5, block5.Value())
+
+	nni6 := uint64(0x010203040506)
+	octet6 := []byte{0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06}
+	block6 := tlv.EncodeNNIBlock(0xAA, nni6)
+	assert.ElementsMatch(t, octet6, block6.Value())
+
+	nni7 := uint64(0x01020304050607)
+	octet7 := []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
+	block7 := tlv.EncodeNNIBlock(0xAA, nni7)
+	assert.ElementsMatch(t, octet7, block7.Value())
+
+	nni8 := uint64(0x0102030405060708)
+	octet8 := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}
+	block8 := tlv.EncodeNNIBlock(0xAA, nni8)
+	assert.ElementsMatch(t, octet8, block8.Value())
+}
