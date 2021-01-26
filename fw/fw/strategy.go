01f2d054f92e0207ed3b836979a644358eb94b0d
Eric Newberry <enewberry@cs.ucla.edu>
2021-01-25 18:26:21 -0700

    Define strategy base

---
+++ fw/fw/strategy.go
@@ -0,0 +1,65 @@
+/* YaNFD - Yet another NDN Forwarding Daemon
+ *
+ * Copyright (C) 2020-2021 Eric Newberry.
+ *
+ * This file is licensed under the terms of the MIT License, as found in LICENSE.md.
+ */
+
+package fw
+
+import (
+	"encoding/binary"
+	"strconv"
+
+	"github.com/eric135/YaNFD/core"
+	"github.com/eric135/YaNFD/dispatch"
+	"github.com/eric135/YaNFD/ndn"
+	"github.com/eric135/YaNFD/table"
+)
+
+// Strategy represents a forwarding strategy.
+type Strategy interface {
+	AfterContentStoreHit(pitEntry *table.PitEntry, inFace int, data *ndn.Data)
+	AfterReceiveData(pitEntry *table.PitEntry, inFace int, data *ndn.Data)
+	AfterReceiveInterest(pitEntry *table.PitEntry, inFace int, interest *ndn.Interest)
+	BeforeSatisfyInterest(pitEntry *table.PitEntry, inFace int, interest *ndn.Interest)
+}
+
+// StrategyBase provides common helper methods for YaNFD forwarding strategies.
+type StrategyBase struct {
+	ThreadID int
+}
+
+// NewStrategyBase is a helper that allows specific strategies to initialize the base.
+func (s *StrategyBase) NewStrategyBase(threadID int) {
+	s.ThreadID = threadID
+}
+
+func (s *StrategyBase) String() string {
+	return "StrategyBase-" + strconv.Itoa(s.ThreadID)
+}
+
+// SendInterest sends an Interest on the specified face.
+func (s *StrategyBase) SendInterest(interest *ndn.Interest, faceID int) {
+	pendingPacket := new(ndn.PendingPacket)
+	pendingPacket.PitToken = make([]byte, 2)
+	binary.BigEndian.PutUint16(pendingPacket.PitToken, uint16(s.ThreadID))
+	var err error
+	pendingPacket.Wire, err = interest.Encode()
+	if err != nil {
+		core.LogWarn(s, "Unable to encode Interest "+interest.Name().String()+" before sending - DROP")
+	}
+	dispatch.GetFace(faceID).SendPacket(pendingPacket)
+}
+
+// SendData sends a Data packet on the specified face.
+func (s *StrategyBase) SendData(data *ndn.Data, inRecord *table.PitInRecord, faceID int) {
+	pendingPacket := new(ndn.PendingPacket)
+	pendingPacket.PitToken = inRecord.PitToken
+	var err error
+	pendingPacket.Wire, err = data.Encode()
+	if err != nil {
+		core.LogWarn(s, "Unable to encode Data "+data.Name().String()+" before sending - DROP")
+	}
+	dispatch.GetFace(faceID).SendPacket(pendingPacket)
+}
