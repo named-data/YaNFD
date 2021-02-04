2ba1821d027cb627242c56935a1f6d7f452710ce
Eric Newberry <enewberry@cs.ucla.edu>
2021-02-03 19:10:57 -0700

    Add multicast strategy

---
+++ fw/fw/multicast.go
@@ -0,0 +1,77 @@
+/* YaNFD - Yet another NDN Forwarding Daemon
+ *
+ * Copyright (C) 2020-2021 Eric Newberry.
+ *
+ * This file is licensed under the terms of the MIT License, as found in LICENSE.md.
+ */
+
+package fw
+
+import (
+	"reflect"
+	"strconv"
+
+	"github.com/eric135/YaNFD/core"
+	"github.com/eric135/YaNFD/ndn"
+	"github.com/eric135/YaNFD/table"
+)
+
+// Multicast is a forwarding strategy that forwards Interests to all nexthop faces.
+type Multicast struct {
+	fib  *table.FibStrategyEntry
+	name *ndn.Name
+	StrategyBase
+}
+
+func init() {
+	strategyTypes = append(strategyTypes, reflect.TypeOf(new(Multicast)))
+}
+
+// Instantiate creates a new instance of the Multicast strategy.
+func (s *Multicast) Instantiate(fib *table.FibStrategyEntry) {
+	s.fib = fib
+	s.name, _ = ndn.NameFromString(StrategyPrefix + "/multicast/%FD%01")
+}
+
+func (s *Multicast) String() string {
+	return "Strategy-Multicast"
+}
+
+// GetName ...
+func (s *Multicast) GetName() *ndn.Name {
+	return s.name
+}
+
+// AfterContentStoreHit ...
+func (s *Multicast) AfterContentStoreHit(pitEntry *table.PitEntry, inFace int, data *ndn.Data) {
+	// Send downstream
+	core.LogTrace(s, "Forwarding content store hit Data "+data.Name().String()+" to "+strconv.Itoa(inFace))
+	s.SendData(data, pitEntry, inFace)
+}
+
+// AfterReceiveData ...
+func (s *Multicast) AfterReceiveData(pitEntry *table.PitEntry, inFace int, data *ndn.Data) {
+	for faceID := range pitEntry.InRecords {
+		core.LogTrace(s, "Forwarding Data "+data.Name().String()+" to "+strconv.Itoa(faceID))
+		s.SendData(data, pitEntry, faceID)
+	}
+}
+
+// AfterReceiveInterest ...
+func (s *Multicast) AfterReceiveInterest(pitEntry *table.PitEntry, inFace int, interest *ndn.Interest) {
+	nexthops := s.fib.LongestPrefixNexthops(interest.Name())
+	if len(nexthops) == 0 {
+		core.LogDebug(s, "No nexthop for Interest "+interest.Name().String()+" - DROP")
+		return
+	}
+
+	for _, nexthop := range nexthops {
+		core.LogTrace(s, "Forwarding Interest "+interest.Name().String()+" to "+strconv.Itoa(nexthop.Nexthop))
+		s.SendInterest(interest, nexthop.Nexthop)
+	}
+}
+
+// BeforeSatisfyInterest ...
+func (s *Multicast) BeforeSatisfyInterest(pitEntry *table.PitEntry, inFace int, data *ndn.Data) {
+	// Does nothing in Multicast
+}
