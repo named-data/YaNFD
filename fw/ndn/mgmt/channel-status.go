09a75e79e523a445524f5d3ecf61f18759c5bef6
Eric Newberry <enewberry@cs.ucla.edu>
2021-02-12 13:19:33 -0800

    Add faces/channels to management

---
+++ fw/ndn/mgmt/channel-status.go
@@ -0,0 +1,33 @@
+/* YaNFD - Yet another NDN Forwarding Daemon
+ *
+ * Copyright (C) 2020-2021 Eric Newberry.
+ *
+ * This file is licensed under the terms of the MIT License, as found in LICENSE.md.
+ */
+
+package mgmt
+
+import (
+	"github.com/eric135/YaNFD/ndn"
+	"github.com/eric135/YaNFD/ndn/tlv"
+)
+
+// ChannelStatus contains status information about a channel.
+type ChannelStatus struct {
+	LocalURI ndn.URI
+}
+
+// MakeChannelStatus creates a ChannelStatus.
+func MakeChannelStatus(localURI *ndn.URI) *ChannelStatus {
+	c := new(ChannelStatus)
+	c.LocalURI = *localURI
+	return c
+}
+
+// Encode encodes a ChannelStatus.
+func (c *ChannelStatus) Encode() (*tlv.Block, error) {
+	wire := tlv.NewEmptyBlock(tlv.ChannelStatus)
+	wire.Append(tlv.NewBlock(tlv.LocalURI, []byte(c.LocalURI.String())))
+	wire.Encode()
+	return wire, nil
+}
