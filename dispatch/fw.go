3bd32ccba701f482e103e36e6430f3e7c673ddbf
Eric Newberry <enewberry@cs.ucla.edu>
2021-04-20 21:16:36 -0700

    Avoid locking in FW thread dispatch. Add optional memory balast

--- dispatch/fw.go
+++ dispatch/fw.go
@@ -7,7 +7,9 @@
 
 package dispatch
 
-import "github.com/eric135/YaNFD/ndn"
+import (
+	"github.com/eric135/YaNFD/ndn"
+)
 
 // FWThread provides an interface that forwarding threads can satisfy (to avoid circular dependency between faces and forwarding)
 type FWThread interface {
@@ -18,4 +20,21 @@ type FWThread interface {
 
 	GetNumPitEntries() int
 	GetNumCsEntries() int
+}
+
+// FWDispatch is used to allow faces to interact with forwarding without a circular dependency issue.
+var FWDispatch []FWThread
+
+// InitializeFWThreads sets up the forwarding thread dispatch slice.
+func InitializeFWThreads(faces []FWThread) {
+	FWDispatch = make([]FWThread, len(faces))
+	copy(FWDispatch, faces)
+}
+
+// GetFWThread returns the specified forwarding thread or nil if it does not exist.
+func GetFWThread(id int) FWThread {
+	if id < 0 || id > len(FWDispatch) {
+		return nil
+	}
+	return FWDispatch[id]
 }
