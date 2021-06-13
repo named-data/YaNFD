63cfd5c8fea3f980d7cb27701bdcc2a54c58f05f
Yash Lala <yashlala@gmail.com>
2021-06-12 17:09:30 -0700

    Add `tcp.lifetime` and `tcp.port_unicast` config options.

--- fw/face/init.go
+++ fw/face/init.go
@@ -44,6 +44,12 @@ var udp6MulticastAddress string
 // udpLifetime is the lifetime of on-demand UDP faces after they become idle.
 var udpLifetime time.Duration
 
+// TCPUnicastPort is the standard unicast TCP port for NDN.
+var TCPUnicastPort uint16
+
+// tcpLifetime is the lifetime of on-demand UDP faces after they become idle.
+var tcpLifetime time.Duration
+
 // UnixSocketPath is the standard Unix socket file path for NDN.
 var UnixSocketPath string
 
@@ -55,9 +61,11 @@ func Configure() {
 	ndnEtherType = core.GetConfigIntDefault("faces.ethernet.ethertype", 0x8624)
 	EthernetMulticastAddress = core.GetConfigStringDefault("faces.ethernet.multicast_address", "01:00:5e:00:17:aa")
 	UDPUnicastPort = core.GetConfigUint16Default("faces.udp.port_unicast", 6363)
+	TCPUnicastPort = core.GetConfigUint16Default("faces.tcp.port_unicast", 6363)
 	UDPMulticastPort = core.GetConfigUint16Default("faces.udp.port_multicast", 56363)
 	udp4MulticastAddress = core.GetConfigStringDefault("faces.udp.multicast_address_ipv4", "224.0.23.170")
 	udp6MulticastAddress = core.GetConfigStringDefault("faces.udp.multicast_address_ipv6", "ff02::114")
 	udpLifetime = time.Duration(core.GetConfigUint16Default("faces.udp.lifetime", 600)) * time.Second
+	tcpLifetime = time.Duration(core.GetConfigUint16Default("faces.tcp.lifetime", 600)) * time.Second
 	UnixSocketPath = os.ExpandEnv(core.GetConfigStringDefault("faces.unix.socket_path", "/run/nfd.sock"))
 }
