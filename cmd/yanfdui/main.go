f7bcfde33295b24624a96108c49f331705bd4ced
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2021-11-30 21:38:19 -0800

    webui: Initial commit for webui

---
+++ cmd/yanfdui/main.go
@@ -0,0 +1,91 @@
+/* YaNFD - Yet another NDN Forwarding Daemon
+ *
+ * Copyright (C) 2020-2021 Eric Newberry.
+ *
+ * This file is licensed under the terms of the MIT License, as found in LICENSE.md.
+ */
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	server "github.com/named-data/YaNFD/cmd/yanfdui/server"
+	"github.com/named-data/YaNFD/core"
+	"github.com/named-data/YaNFD/executor"
+)
+
+// Version of YaNFD.
+var Version string
+
+// Addr of HTTP server.
+var Addr string = "localhost:5010"
+
+func main() {
+	// Parse command line options
+	var shouldPrintVersion bool
+	flag.BoolVar(&shouldPrintVersion, "version", false, "Print version and exit")
+	var configFileName string
+	flag.StringVar(&configFileName, "config", "/usr/local/etc/ndn/yanfd.toml", "Configuration file location")
+	var disableEthernet bool
+	flag.BoolVar(&disableEthernet, "disable-ethernet", false, "Disable Ethernet transports (deprecated; set.faces.ethernet.enabled=false in config file instead)")
+	var disableUnix bool
+	flag.BoolVar(&disableUnix, "disable-unix", false, "Disable Unix stream transports (deprecated; set.faces.unix.enabled=false in config file instead)")
+	var cpuProfile string
+	flag.StringVar(&cpuProfile, "cpu-profile", "", "Enable CPU profiling (output to specified file)")
+	var memProfile string
+	flag.StringVar(&memProfile, "mem-profile", "", "Enable memory profiling (output to specified file)")
+	var blockProfile string
+	flag.StringVar(&blockProfile, "block-profile", "", "Enable block profiling (output to specified file)")
+	var memoryBallastSize int
+	flag.IntVar(&memoryBallastSize, "memory-ballast", 0, "Enable memory ballast of specified size (in GB) to avoid frequent garbage collection")
+	flag.Parse()
+
+	if shouldPrintVersion {
+		fmt.Println("YaNFD: Yet another NDN Forwarding Daemon")
+		fmt.Println("Version " + Version)
+		fmt.Println("Copyright (C) 2020-2021 Eric Newberry")
+		fmt.Println("Released under the terms of the MIT License")
+		return
+	}
+
+	config := executor.YaNFDConfig{
+		Version:           Version,
+		ConfigFileName:    configFileName,
+		DisableEthernet:   disableEthernet,
+		DisableUnix:       disableUnix,
+		LogFile:           "",
+		CpuProfile:        cpuProfile,
+		MemProfile:        memProfile,
+		BlockProfile:      blockProfile,
+		MemoryBallastSize: memoryBallastSize,
+	}
+
+	yanfd := executor.NewYaNFD(&config)
+	yanfd.Start()
+
+	// Start HTTP server
+	httpServerExitDone := &sync.WaitGroup{}
+	httpServerExitDone.Add(1)
+	srv := server.StartHttpServer(httpServerExitDone, Addr)
+
+	// Set up signal handler channel and wait for interrupt
+	core.LogInfo("Main", "HTTP server started, serving at http://", Addr)
+	sigChannel := make(chan os.Signal, 1)
+	signal.Notify(sigChannel, os.Interrupt, syscall.SIGTERM)
+	receivedSig := <-sigChannel
+	core.LogInfo("Main", "Received signal ", receivedSig, " - exiting")
+
+	// Stop HTTP server
+	if err := srv.Shutdown(context.Background()); err != nil {
+		core.LogInfo("Main", "Error in shutting down HTTP server: ", err)
+	}
+	yanfd.Stop()
+	httpServerExitDone.Wait()
+}
