dbb5d913eaf66b701bdbf443f871ce40274e56c2
Varun Patil <varunpatil@ucla.edu>
2024-12-18 09:20:32 +0000

    ra: count announcements

--- mgmt/nlsr_readvertiser.go
+++ mgmt/nlsr_readvertiser.go
@@ -1,6 +1,8 @@
 package mgmt
 
 import (
+	"sync"
+
 	"github.com/named-data/YaNFD/core"
 	"github.com/named-data/YaNFD/table"
 	enc "github.com/zjkmxy/go-ndn/pkg/encoding"
@@ -10,17 +12,38 @@ import (
 
 // Simple readvertiser that echoes the register command to NLSR.
 // Currently the command is one-shot, and does not handle failures.
-type ReadvertiseNlsr struct {
+type NlsrReadvertiser struct {
 	m *Thread
+	// List of routes already advertised to NLSR
+	advertised map[uint64]int // hash -> count
+	// This is called from RIB (i.e. could be fw threads)
+	mutex sync.Mutex
 }
 
-func (r *ReadvertiseNlsr) String() string {
-	return "ReadvertiseNlsr"
+func NewNlsrReadvertiser(m *Thread) *NlsrReadvertiser {
+	return &NlsrReadvertiser{
+		m:          m,
+		advertised: make(map[uint64]int),
+	}
 }
 
-func (r *ReadvertiseNlsr) Announce(name enc.Name, route *table.Route) {
+func (r *NlsrReadvertiser) String() string {
+	return "NlsrReadvertiser"
+}
+
+func (r *NlsrReadvertiser) Announce(name enc.Name, route *table.Route) {
 	if route.Origin != table.RouteOriginClient {
 		core.LogDebug(r, "skip advertise=", name, " origin=", route.Origin)
+		return
+	}
+
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	nhash := name.Hash()
+	r.advertised[nhash] += 1
+	if r.advertised[nhash] > 1 {
+		core.LogDebug(r, "skip advertise=", name, " already advertised")
 		return
 	}
 	core.LogInfo(r, "advertise=", name)
@@ -42,9 +65,19 @@ func (r *ReadvertiseNlsr) Announce(name enc.Name, route *table.Route) {
 	r.m.sendInterest(cmd, params.Encode())
 }
 
-func (r *ReadvertiseNlsr) Withdraw(name enc.Name, route *table.Route) {
+func (r *NlsrReadvertiser) Withdraw(name enc.Name, route *table.Route) {
 	if route.Origin != table.RouteOriginClient {
 		core.LogDebug(r, "skip withdraw=", name, " origin=", route.Origin)
+		return
+	}
+
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	nhash := name.Hash()
+	r.advertised[nhash] -= 1
+	if r.advertised[nhash] > 0 {
+		core.LogDebug(r, "skip withdraw=", name, " still advertised")
 		return
 	}
 	core.LogInfo(r, "withdraw=", name)
